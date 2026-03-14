28baf06222cef978400490b3a3b1d599cf3301b9
Argavyon <argavyon@gmail.com>
2026-03-14 15:49:19 +0100

    Improve Affix Groups & Synonym list
    Re-write the lists of groups and synonyms in a declarative style. This should be easier to read and understand.

--- data-builder/build_affix_groups.py
+++ data-builder/build_affix_groups.py
@@ -8,56 +8,48 @@ def get_all_saves(bonusType = None) -> list[str]:
 def get_all_skills(bonusType = None) -> list[str]:
     return ['Balance', 'Bluff', 'Concentration', 'Diplomacy', 'Disable Device', 'Fortitude Save', 'Spot', 'Haggle', 'Heal', 'Hide', 'Intimidate', 'Jump', 'Listen', 'Move Silently', 'Open Lock', 'Perform', 'Reflex Save', 'Repair', 'Resistance', 'Search', 'Spellcraft', 'Spot', 'Swim', 'Tumble', 'Will Save', 'Use Magic Device']
 
-
-def add(groups: list[AffixGroup], name: str, affixes: list[str]) -> None:
-    groups.append({
-        'name': name,
-        'affixes': affixes
-    })
-
-
 def build_affix_groups() -> None:
-    groups: list[AffixGroup] = []
-
     parrying = get_all_saves()
     parrying.append('Armor Class')
 
-    # Technically Armor/Weapon Enhancement Bonuses add to AC / Accuracy & Deadly, but we'd need to fake a channel for them
-    # and I don't really care about them.
-    #add(groups, 'Enhancement Bonus (Armor)', ['Armor Class'])
-    #add(groups, 'Enhancement Bonus (Weapon)', ['Accuracy', 'Deadly'])
-    add(groups, 'Good Luck', ['Resistance'] + get_all_saves() + get_all_skills())
-    add(groups, 'Negative and Poison Spell Crit Damage', ['Negative Spell Crit Damage', 'Poison Spell Crit Damage'])
-    add(groups, 'Resistance', get_all_saves())
-    add(groups, 'Riposte', ['Armor Class', 'Resistance'] + get_all_saves())
-    # special case exists where Litany of the Dead Ability Bonus is really well rounded affix
-    # but we treat as an affix group to keep consistency with Litany of the Dead Combat Bonus affix
-    add(groups, 'Litany of the Dead - Ability Bonus', ['Well Rounded'])
-    add(groups, 'Litany of the Dead II - Ability Bonus', ['Well Rounded'])
-    add(groups, 'Litany of the Dead - Combat Bonus', ['Accuracy', 'Deadly'])
-    add(groups, 'Litany of the Dead II - Combat Bonus', ['Accuracy', 'Deadly'])
-    add(groups, 'Parrying', parrying)
-    add(groups, 'Sheltering', ['Physical Sheltering', 'Magical Sheltering'])
-    add(groups, 'Potency', ['Nullification', 'Radiance', 'Devotion', 'Corrosion', 'Combustion', 'Magnetic', 'Glaciation', 'Reconstruction', 'Impulse', 'Resonance'])
-    add(groups, 'Spell Lore', ['Nullification Lore', 'Radiance Lore', 'Devotion Lore', 'Corrosion Lore', 'Combustion Lore', 'Magnetic Lore', 'Glaciation Lore', 'Reconstruction Lore', 'Impulse Lore', 'Resonance'])
-    add(groups, 'Combat Mastery', ['Vertigo', 'Stunning', 'Dazing', 'Sundering', 'Shatter'])
-    add(groups, 'Alluring Skills Bonus', ['Bluff', 'Diplomacy', 'Haggle', 'Intimidate', 'Perform'])
-    add(groups, 'Charisma Skills', ['Bluff', 'Diplomacy', 'Haggle', 'Intimidate', 'Perform'])
-    add(groups, 'Frozen Depths Lore', ['Ice Lore', 'Poison Lore', 'Void Lore'])
-    add(groups, 'Frozen Storm Lore', ['Ice Lore', 'Lightning Lore'])
-    add(groups, 'Intelligence Skills', ['Disable Device', 'Repair', 'Search', 'Spellcraft'])
-    add(groups, 'Dexterity Skills', ['Balance', 'Hide', 'Move Silently', 'Open Locks', 'Tumble'])
-    add(groups, 'Power of the Frozen Storm', ['Glaciation', 'Magnetism'])
-    add(groups, 'Power of the Frozen Depths', ['Glaciation', 'Nullification', 'Poison'])
-    add(groups, 'Power of the Flames of Purity', ['Combustion', 'Radiance'])
-    add(groups, 'Power of the Silver Flame', ['Devotion', 'Radiance'])
-    add(groups, 'Purifying Flame Lore', ['Fire Lore', 'Radiance Lore'])
-    add(groups, 'Strength Skills', ['Jump'])
-    add(groups, 'Wisdom Skills', ['Heal', 'Listen', 'Spot'])
-    add(groups, 'Constitution Skills', ['Concentration'])
-    add(groups, 'Well Rounded', ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma'])
-    add(groups, 'Spell Focus Mastery', ['Evocation Focus', 'Necromancy Focus', 'Transmutation Focus', 'Enchantment Focus', 'Conjuration Focus', 'Abjuration Focus', 'Illusion Focus'])
-    add(groups, 'each Amplification', ['Healing Amplification', 'Negative Amplification', 'Repair Amplification'])
+    groups: list[AffixGroup] = [
+        # Technically Armor/Weapon Enhancement Bonuses add to AC / Accuracy & Deadly, but we'd need to fake a channel for them
+        # and I don't really care about them.
+        # AffixGroup(name = 'Enhancement Bonus (Armor)', affixes = ['Armor Class']),
+        # AffixGroup(name = 'Enhancement Bonus (Weapon)', affixes = ['Accuracy', 'Deadly']),
+        AffixGroup(name = 'Good Luck', affixes = ['Resistance'] + get_all_saves() + get_all_skills()),
+        AffixGroup(name = 'Negative and Poison Spell Crit Damage', affixes = ['Negative Spell Crit Damage', 'Poison Spell Crit Damage']),
+        AffixGroup(name = 'Resistance', affixes = get_all_saves()),
+        AffixGroup(name = 'Riposte', affixes = ['Armor Class', 'Resistance'] + get_all_saves()),
+        # special case exists where Litany of the Dead Ability Bonus is really well rounded affix
+        # but we treat as an affix group to keep consistency with Litany of the Dead Combat Bonus affix
+        AffixGroup(name = 'Litany of the Dead - Ability Bonus', affixes = ['Well Rounded']),
+        AffixGroup(name = 'Litany of the Dead II - Ability Bonus', affixes = ['Well Rounded']),
+        AffixGroup(name = 'Litany of the Dead - Combat Bonus', affixes = ['Accuracy', 'Deadly']),
+        AffixGroup(name = 'Litany of the Dead II - Combat Bonus', affixes = ['Accuracy', 'Deadly']),
+        AffixGroup(name = 'Parrying', affixes = parrying),
+        AffixGroup(name = 'Sheltering', affixes = ['Physical Sheltering', 'Magical Sheltering']),
+        AffixGroup(name = 'Potency', affixes = ['Nullification', 'Radiance', 'Devotion', 'Corrosion', 'Combustion', 'Magnetic', 'Glaciation', 'Reconstruction', 'Impulse', 'Resonance']),
+        AffixGroup(name = 'Spell Lore', affixes = ['Nullification Lore', 'Radiance Lore', 'Devotion Lore', 'Corrosion Lore', 'Combustion Lore', 'Magnetic Lore', 'Glaciation Lore', 'Reconstruction Lore', 'Impulse Lore', 'Resonance']),
+        AffixGroup(name = 'Combat Mastery', affixes = ['Vertigo', 'Stunning', 'Dazing', 'Sundering', 'Shatter']),
+        AffixGroup(name = 'Alluring Skills Bonus', affixes = ['Bluff', 'Diplomacy', 'Haggle', 'Intimidate', 'Perform']),
+        AffixGroup(name = 'Charisma Skills', affixes = ['Bluff', 'Diplomacy', 'Haggle', 'Intimidate', 'Perform']),
+        AffixGroup(name = 'Frozen Depths Lore', affixes = ['Ice Lore', 'Poison Lore', 'Void Lore']),
+        AffixGroup(name = 'Frozen Storm Lore', affixes = ['Ice Lore', 'Lightning Lore']),
+        AffixGroup(name = 'Intelligence Skills', affixes = ['Disable Device', 'Repair', 'Search', 'Spellcraft']),
+        AffixGroup(name = 'Dexterity Skills', affixes = ['Balance', 'Hide', 'Move Silently', 'Open Locks', 'Tumble']),
+        AffixGroup(name = 'Power of the Frozen Storm', affixes = ['Glaciation', 'Magnetism']),
+        AffixGroup(name = 'Power of the Frozen Depths', affixes = ['Glaciation', 'Nullification', 'Poison']),
+        AffixGroup(name = 'Power of the Flames of Purity', affixes = ['Combustion', 'Radiance']),
+        AffixGroup(name = 'Power of the Silver Flame', affixes = ['Devotion', 'Radiance']),
+        AffixGroup(name = 'Purifying Flame Lore', affixes = ['Fire Lore', 'Radiance Lore']),
+        AffixGroup(name = 'Strength Skills', affixes = ['Jump']),
+        AffixGroup(name = 'Wisdom Skills', affixes = ['Heal', 'Listen', 'Spot']),
+        AffixGroup(name = 'Constitution Skills', affixes = ['Concentration']),
+        AffixGroup(name = 'Well Rounded', affixes = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 'Charisma']),
+        AffixGroup(name = 'Spell Focus Mastery', affixes = ['Evocation Focus', 'Necromancy Focus', 'Transmutation Focus', 'Enchantment Focus', 'Conjuration Focus', 'Abjuration Focus', 'Illusion Focus']),
+        AffixGroup(name = 'each Amplification', affixes = ['Healing Amplification', 'Negative Amplification', 'Repair Amplification']),
+    ]
 
     write_json(groups, 'affix-groups')
 
--- data-builder/build_synonyms.py
+++ data-builder/build_synonyms.py
@@ -2,58 +2,51 @@ from write_json import write_json
 
 from typedefs import AffixSynonyms
 
-def add(data: list[AffixSynonyms], mainName: str, synonyms: list[str]) -> None:
-    data.append({
-        'name': mainName,
-        'synonyms': synonyms,
-    })
-
-
 def build_synonyms() -> None:
-    data: list[AffixSynonyms] = []
-
-    add(data, 'Accuracy', ['Attack', 'Hit', 'hit', 'Attack Bonus'])
-    add(data, 'Action Boost Charges', ['Action Boost Enhancement'])
-    add(data, 'Arcane Casting Dexterity', ['Lesser Arcane Casting Dexterity', 'Greater Arcane Casting Dexterity'])
-    add(data, 'Armor Class', ['AC', 'Armor Bonus', 'Natural Armor', 'Natural Armor Bonus', 'Protection', 'Rough Hide', 'Shield', 'Shield Armor Class'])
-    # probably want to standardize on 'Armor Piercing' as the name, but re-work needs to be done on cannith crafting to remove drift
-    add(data, 'Armor-Piercing', ['Armor Piercing', 'Fortification Bypass', 'Fortification bypass'])
-    add(data, 'Assassinate', ['Assassinate DCs'])
-    add(data, 'Deadly', ['Damage', 'Damage Bonus'])
-    add(data, 'Devotion', ['Positive Spell Power', 'Positive Spellpower'])
-    add(data, 'Evocation Focus', ['Evocation Spell DCs'])
-    add(data, 'False Life', ['Hit Points', 'Lifeforce', 'Maximum HP', 'Maximum Hit Points', 'maximum hitpoints', 'Vitality', 'your maximum hit points'])
-    add(data, 'Force Spell Crit Damage', ['Force and Physical Spell Crit Damage'])
-    add(data, 'Greater Dragonmark Charges', ['Greater Dragonmark Enhancement'])
-    add(data, 'Healing Lore', ['Positive Spell Crit Chance', 'Positive Spellcrit Chance'])
-    add(data, 'Lesser Dragonmark Charges', ['Lesser Dragonmark Enhancement'])
-    add(data, 'Light Spell Crit Damage', ['Light and Alignment Spell Crit Damage'])
-    add(data, 'Seeker', ['Critical Confirmation and Critical Damage'])
-    add(data, 'Void Lore', ['Negative Spell Crit Chance', 'Negative Lore'])
-    add(data, 'Deception', ['Sneak Attacks'])
-    add(data, 'Speed', ['Striding', 'movement speed'])
-    add(data, 'Physical Sheltering', ['Physical Resistance Rating', 'PRR'])
-    add(data, 'Magical Sheltering', ['Magical Resistance Rating', 'MRR', 'your Magical Resistance Rating'])
-    add(data, 'Magical Sheltering Cap', ['Magical Resistance Rating Cap', 'MRR Cap'])
-    add(data, 'Rage Charges', ['Anger', 'Minor Anger'])
-    add(data, 'Sheltering', ['Physical and Magical Resistance Rating'])
-    add(data, 'Smite Evil Charges', ['Extra Smites'])
-    add(data, 'Spell Focus Mastery', ['DCs', 'Spell DCs', 'all Spell DCs', 'all spell DCs', 'Spell DC\'s'])
-    add(data, 'Spell Points', ['your maximum Spell Points'])
-    add(data, 'Stunning', ['Stunning DCs'])
-    add(data, 'Sunder', ['Sunder DCs'])
-    add(data, 'Tactical Abilities', ['your Tactical Abilities'])
-    add(data, 'Trip', ['Trip DCs'])
-    add(data, 'Universal Spell Power', ['Spellcasting Implement', 'Universal Spellpower'])
-    add(data, 'Nullification', ['Negative Spell Power'])
-    add(data, 'Healing Amplification', ['Positive Healing Amplification', 'Positive Amplification'])
-    add(data, 'Negative Amplification', ['Negative Healing Amplification'])
-    add(data, 'Repair Amplification', ['Repair Healing Amplification'])
-    add(data, 'Well Rounded', ['Ability stats', 'All Ability Scores', 'all Ability Scores', 'all of your Ability Scores', 'each Ability Score', 'Well-Rounded'])
-    add(data, 'Sundering', ['Sunder', 'Sunder DC'])
-    add(data, 'Vertigo', ['Trip', 'Trip DC'])
-    add(data, 'Silver', ['Silver , Alchemical'])
-    add(data, 'Arcane Casting Dexterity', ['reduce your Arcane Spell Failure by', 'Arcane Spell Failure'])
+    data: list[AffixSynonyms] = [
+        AffixSynonyms(name = 'Accuracy', synonyms = ['Attack', 'Hit', 'hit', 'Attack Bonus']),
+        AffixSynonyms(name = 'Action Boost Charges', synonyms = ['Action Boost Enhancement']),
+        AffixSynonyms(name = 'Arcane Casting Dexterity', synonyms = ['Lesser Arcane Casting Dexterity', 'Greater Arcane Casting Dexterity']),
+        AffixSynonyms(name = 'Armor Class', synonyms = ['AC', 'Armor Bonus', 'Natural Armor', 'Natural Armor Bonus', 'Protection', 'Rough Hide', 'Shield', 'Shield Armor Class']),
+        # probably want to standardize on 'Armor Piercing' as the name, but re-work needs to be done on cannith crafting to remove drift
+        AffixSynonyms(name = 'Armor-Piercing', synonyms = ['Armor Piercing', 'Fortification Bypass', 'Fortification bypass']),
+        AffixSynonyms(name = 'Assassinate', synonyms = ['Assassinate DCs']),
+        AffixSynonyms(name = 'Deadly', synonyms = ['Damage', 'Damage Bonus']),
+        AffixSynonyms(name = 'Devotion', synonyms = ['Positive Spell Power', 'Positive Spellpower']),
+        AffixSynonyms(name = 'Evocation Focus', synonyms = ['Evocation Spell DCs']),
+        AffixSynonyms(name = 'False Life', synonyms = ['Hit Points', 'Lifeforce', 'Maximum HP', 'Maximum Hit Points', 'maximum hitpoints', 'Vitality', 'your maximum hit points']),
+        AffixSynonyms(name = 'Force Spell Crit Damage', synonyms = ['Force and Physical Spell Crit Damage']),
+        AffixSynonyms(name = 'Greater Dragonmark Charges', synonyms = ['Greater Dragonmark Enhancement']),
+        AffixSynonyms(name = 'Healing Lore', synonyms = ['Positive Spell Crit Chance', 'Positive Spellcrit Chance']),
+        AffixSynonyms(name = 'Lesser Dragonmark Charges', synonyms = ['Lesser Dragonmark Enhancement']),
+        AffixSynonyms(name = 'Light Spell Crit Damage', synonyms = ['Light and Alignment Spell Crit Damage']),
+        AffixSynonyms(name = 'Seeker', synonyms = ['Critical Confirmation and Critical Damage']),
+        AffixSynonyms(name = 'Void Lore', synonyms = ['Negative Spell Crit Chance', 'Negative Lore']),
+        AffixSynonyms(name = 'Deception', synonyms = ['Sneak Attacks']),
+        AffixSynonyms(name = 'Speed', synonyms = ['Striding', 'movement speed']),
+        AffixSynonyms(name = 'Physical Sheltering', synonyms = ['Physical Resistance Rating', 'PRR']),
+        AffixSynonyms(name = 'Magical Sheltering', synonyms = ['Magical Resistance Rating', 'MRR', 'your Magical Resistance Rating']),
+        AffixSynonyms(name = 'Magical Sheltering Cap', synonyms = ['Magical Resistance Rating Cap', 'MRR Cap']),
+        AffixSynonyms(name = 'Rage Charges', synonyms = ['Anger', 'Minor Anger']),
+        AffixSynonyms(name = 'Sheltering', synonyms = ['Physical and Magical Resistance Rating']),
+        AffixSynonyms(name = 'Smite Evil Charges', synonyms = ['Extra Smites']),
+        AffixSynonyms(name = 'Spell Focus Mastery', synonyms = ['DCs', 'Spell DCs', 'all Spell DCs', 'all spell DCs', 'Spell DC\'s']),
+        AffixSynonyms(name = 'Spell Points', synonyms = ['your maximum Spell Points']),
+        AffixSynonyms(name = 'Stunning', synonyms = ['Stunning DCs']),
+        AffixSynonyms(name = 'Sunder', synonyms = ['Sunder DCs']),
+        AffixSynonyms(name = 'Tactical Abilities', synonyms = ['your Tactical Abilities']),
+        AffixSynonyms(name = 'Trip', synonyms = ['Trip DCs']),
+        AffixSynonyms(name = 'Universal Spell Power', synonyms = ['Spellcasting Implement', 'Universal Spellpower']),
+        AffixSynonyms(name = 'Nullification', synonyms = ['Negative Spell Power']),
+        AffixSynonyms(name = 'Healing Amplification', synonyms = ['Positive Healing Amplification', 'Positive Amplification']),
+        AffixSynonyms(name = 'Negative Amplification', synonyms = ['Negative Healing Amplification']),
+        AffixSynonyms(name = 'Repair Amplification', synonyms = ['Repair Healing Amplification']),
+        AffixSynonyms(name = 'Well Rounded', synonyms = ['Ability stats', 'All Ability Scores', 'all Ability Scores', 'all of your Ability Scores', 'each Ability Score', 'Well-Rounded']),
+        AffixSynonyms(name = 'Sundering', synonyms = ['Sunder', 'Sunder DC']),
+        AffixSynonyms(name = 'Vertigo', synonyms = ['Trip', 'Trip DC']),
+        AffixSynonyms(name = 'Silver', synonyms = ['Silver , Alchemical']),
+        AffixSynonyms(name = 'Arcane Casting Dexterity', synonyms = ['reduce your Arcane Spell Failure by', 'Arcane Spell Failure']),
+    ]
 
     write_json(data, 'affix-synonyms')
 
