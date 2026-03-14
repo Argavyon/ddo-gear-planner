33570208e6934a3f7ddbbf0b4926d05ce55d6f22
Argavyon <argavyon@gmail.com>
2026-03-14 15:55:05 +0100

    Improve Affix Groups
    Re-write the "get_all_*" functions in a declarative style.

--- data-builder/build_affix_groups.py
+++ data-builder/build_affix_groups.py
@@ -2,32 +2,26 @@ from write_json import write_json
 
 from typedefs import AffixGroup
 
-def get_all_saves(bonusType = None) -> list[str]:
-    return ['Fortitude Save', 'Reflex Save', 'Will Save']
-
-def get_all_skills(bonusType = None) -> list[str]:
-    return ['Balance', 'Bluff', 'Concentration', 'Diplomacy', 'Disable Device', 'Fortitude Save', 'Spot', 'Haggle', 'Heal', 'Hide', 'Intimidate', 'Jump', 'Listen', 'Move Silently', 'Open Lock', 'Perform', 'Reflex Save', 'Repair', 'Resistance', 'Search', 'Spellcraft', 'Spot', 'Swim', 'Tumble', 'Will Save', 'Use Magic Device']
-
 def build_affix_groups() -> None:
-    parrying = get_all_saves()
-    parrying.append('Armor Class')
+    all_saves = ['Fortitude Save', 'Reflex Save', 'Will Save']
+    all_skills = ['Balance', 'Bluff', 'Concentration', 'Diplomacy', 'Disable Device', 'Fortitude Save', 'Spot', 'Haggle', 'Heal', 'Hide', 'Intimidate', 'Jump', 'Listen', 'Move Silently', 'Open Lock', 'Perform', 'Reflex Save', 'Repair', 'Resistance', 'Search', 'Spellcraft', 'Spot', 'Swim', 'Tumble', 'Will Save', 'Use Magic Device']
 
     groups: list[AffixGroup] = [
         # Technically Armor/Weapon Enhancement Bonuses add to AC / Accuracy & Deadly, but we'd need to fake a channel for them
         # and I don't really care about them.
         # AffixGroup(name = 'Enhancement Bonus (Armor)', affixes = ['Armor Class']),
         # AffixGroup(name = 'Enhancement Bonus (Weapon)', affixes = ['Accuracy', 'Deadly']),
-        AffixGroup(name = 'Good Luck', affixes = ['Resistance'] + get_all_saves() + get_all_skills()),
+        AffixGroup(name = 'Good Luck', affixes = ['Resistance', *all_saves, *all_skills]),
         AffixGroup(name = 'Negative and Poison Spell Crit Damage', affixes = ['Negative Spell Crit Damage', 'Poison Spell Crit Damage']),
-        AffixGroup(name = 'Resistance', affixes = get_all_saves()),
-        AffixGroup(name = 'Riposte', affixes = ['Armor Class', 'Resistance'] + get_all_saves()),
+        AffixGroup(name = 'Resistance', affixes = all_saves),
+        AffixGroup(name = 'Riposte', affixes = ['Armor Class', 'Resistance', *all_saves]),
         # special case exists where Litany of the Dead Ability Bonus is really well rounded affix
         # but we treat as an affix group to keep consistency with Litany of the Dead Combat Bonus affix
         AffixGroup(name = 'Litany of the Dead - Ability Bonus', affixes = ['Well Rounded']),
         AffixGroup(name = 'Litany of the Dead II - Ability Bonus', affixes = ['Well Rounded']),
         AffixGroup(name = 'Litany of the Dead - Combat Bonus', affixes = ['Accuracy', 'Deadly']),
         AffixGroup(name = 'Litany of the Dead II - Combat Bonus', affixes = ['Accuracy', 'Deadly']),
-        AffixGroup(name = 'Parrying', affixes = parrying),
+        AffixGroup(name = 'Parrying', affixes = ['Armor Class', *all_saves]),
         AffixGroup(name = 'Sheltering', affixes = ['Physical Sheltering', 'Magical Sheltering']),
         AffixGroup(name = 'Potency', affixes = ['Nullification', 'Radiance', 'Devotion', 'Corrosion', 'Combustion', 'Magnetic', 'Glaciation', 'Reconstruction', 'Impulse', 'Resonance']),
         AffixGroup(name = 'Spell Lore', affixes = ['Nullification Lore', 'Radiance Lore', 'Devotion Lore', 'Corrosion Lore', 'Combustion Lore', 'Magnetic Lore', 'Glaciation Lore', 'Reconstruction Lore', 'Impulse Lore', 'Resonance']),
